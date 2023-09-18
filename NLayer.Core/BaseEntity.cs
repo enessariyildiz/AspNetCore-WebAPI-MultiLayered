d8c05b3c4106a34a18ff0da14fb9e80b04763d64
Enes Sarıyıldız <ens.sryldz@gmail.com>
2023-09-18 19:50:57 +0300

    First Commit

---
+++ NLayer.Core/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core
+{
+    public abstract class BaseEntity
+    {
+        // Bu entityden bir nesne örneği alınmaması için abstract class yapıyoruz. Çünkü bu entity bir base yapıdır.
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
+ 
