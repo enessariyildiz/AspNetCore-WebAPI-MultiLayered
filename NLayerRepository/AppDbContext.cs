086061f1517f64c1800a2bb552b241eeab2c521f
Enes Sarıyıldız <ens.sryldz@gmail.com>
2023-09-19 16:10:32 +0300

    Entity Configurations done

---
+++ NLayerRepository/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core;
+using System.Reflection;
+
+namespace NLayerRepository
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            // Options almasının sebebi; db yolunu startup dosyasından alacağı için.
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> ProductFeatures { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
