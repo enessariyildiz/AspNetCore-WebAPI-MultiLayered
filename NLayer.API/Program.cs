6d7661a481e3115442304f224b0447082263aba5
Enes Sarıyıldız <ens.sryldz@gmail.com>
2023-09-20 17:33:13 +0300

    AutoMapper library and configuration done.

--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.EntityFrameworkCore;
 using NLayer.Core.Repositories;
 using NLayer.Core.Services;
 using NLayer.Core.UnitOfWorks;
+using NLayer.Service.Mapping;
+using NLayer.Service.Services;
 using NLayerRepository;
 using NLayerRepository.Repositories;
 using NLayerRepository.UnitOfWorks;
@@ -16,10 +18,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Kursla beraber eklenen satýrlar.
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped(typeof(IService<>), typeof(Service<>));
 
-//builder.Services.AddScoped(typeof(IService<>), typeof(Service<>));
+builder.Services.AddAutoMapper(typeof(MapProfile));
+
+
 
 builder.Services.AddDbContext<AppDbContext>(x =>
  {
