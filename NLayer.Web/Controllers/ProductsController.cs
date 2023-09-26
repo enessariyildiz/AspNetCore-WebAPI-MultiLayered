ed3a0e537d012c6b2d20d83a584efc55e7ad26c2
Enes Sarıyıldız <ens.sryldz@gmail.com>
2023-09-26 17:40:41 +0300

    Added Products Controller Save-1

--- NLayer.Web/Controllers/ProductsController.cs
+++ NLayer.Web/Controllers/ProductsController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NLayer.Core.DTOs;
+using NLayer.Core.Models;
 using NLayer.Core.Services;
 
 namespace NLayer.Web.Controllers
@@ -7,15 +11,50 @@ namespace NLayer.Web.Controllers
     {
 
         private readonly IProductService _services;
+        private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
 
-        public ProductsController(IProductService services)
+        public ProductsController(IProductService services, ICategoryService categoryService, IMapper mapper)
         {
             _services = services;
+            _categoryService = categoryService;
+            _mapper = mapper;
         }
 
         public async Task<IActionResult> Index()
         {
             return View(await _services.GetProductsWithCategory());
+        }
+
+        public async Task<IActionResult> Save()
+        {
+            var categories = _categoryService.GetAllAsync();
+
+            var categoryDto = _mapper.Map<List<CategoryDto>>(categories);
+
+            ViewBag.categories = new SelectList(categoryDto, "Id", "Name");
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(ProductDto productDto)
+        {
+
+            if (ModelState.IsValid)
+            {
+                await _services.AddAsync(_mapper.Map<Product>(productDto));
+                return RedirectToAction(nameof(Index));
+            }
+
+            var categories = _categoryService.GetAllAsync();
+
+            var categoryDto = _mapper.Map<List<CategoryDto>>(categories);
+
+            ViewBag.categories = new SelectList(categoryDto, "Id", "Name");
+
+
+            return View();
         }
     }
 }
