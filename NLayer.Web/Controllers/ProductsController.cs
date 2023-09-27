910178f66834ef2b461f68ce3409d4265e70533b
Enes Sarıyıldız <ens.sryldz@gmail.com>
2023-09-27 22:04:40 +0300

    Added Products Contoller Delete

--- NLayer.Web/Controllers/ProductsController.cs
+++ NLayer.Web/Controllers/ProductsController.cs
@@ -72,5 +72,14 @@ namespace NLayer.Web.Controllers
             ViewBag.categories = new SelectList(categoriesDto, "Id", "Name", productDto.CategoryId);
             return View(productDto);
         }
+
+        public async Task<IActionResult> Remove(int id)
+        {
+            var product = await _services.GetByIdAsync(id);
+
+            await _services.RemovAsync(product);
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
